8e9d224d4c4ecd597695e67404b829b4e0a78504
mami0tsu <mami0tsu.jp@gmail.com>
2022-07-21 23:58:12 +0900

    Change font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
 	use_ime = true,
     font_dirs = {"fonts"},
 	font = wezterm.font("FirgeNerd Console", {weight="Regular", stretch="Normal", style="Normal"}),
- 	font_size = 18.0,
+ 	font_size = 16.0,
 	color_scheme = "Wombat",
 	hide_tab_bar_if_only_one_tab = true,
 	adjust_window_size_when_changing_font_size = false,
