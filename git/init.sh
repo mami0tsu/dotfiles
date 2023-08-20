8c49a16f432b4fb16a583a5a03797fa0598ce246
mami0tsu <mami0tsu.jp@gmail.com>
2023-08-20 22:09:49 +0900

    fix: switch make to go-task

--- git/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-read USER_NAME\?'input user.name (e.g. username) > '
-if test -n "${USER_NAME}"; then git config --global user.name "\"${USER_NAME}\""; fi
-read USER_EMAIL\?'input user.email (e.g. username@example.com) > '
-if test -n "${USER_EMAIL}"; then git config --global user.email "\"${USER_EMAIL}\""; fi
