626f248c2e2e159a3b9bca6073ccf2b50620ddd2
mami0tsu <mami0tsu.jp@gmail.com>
2026-04-12 17:01:26 +0900

    feat(hm): migrate tools from mise and homebrew to home-manager packages

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -11,7 +11,38 @@
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
-    ponysay
+    bat
+    coreutils
+    curl
+    delta
+    eza
+    findutils
+    gnugrep
+    gnused
+    gojq
+    # jq
+    tree
+    ripgrep
+
+    awscli2
+    docker
+    fzf
+    gh
+    ghq
+    git-secrets
+    go-task
+    neovim
+    sheldon
+    starship
+    tree-sitter
+    zellij
+
+    deno
+    fnm
+    go
+    rustup
+    tenv
+    uv
   ];
 
   programs.home-manager.enable = true;
