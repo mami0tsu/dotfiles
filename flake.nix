0ce480f15f2b5d008c74a05aa97a6b5f718aa5a1
mami0tsu <mami0tsu.jp@gmail.com>
2026-04-25 18:29:34 +0900

    cleanup: remove unused local.nix logic from flake.nix

--- flake.nix
+++ flake.nix
@@ -32,20 +32,6 @@
       ...
     }@inputs:
     let
-      # local.nix が存在する場合は読み込み、存在しない場合は空のセットを返す
-      localConfig = if builtins.pathExists ./local.nix then import ./local.nix else { };
-
-      # ユーザー名が指定されていない場合にエラーを表示する
-      username =
-        localConfig.username
-          or (throw ''
-            Error: 'local.nix' is missing or 'username' is not defined.
-            Please create 'local.nix' with the following content:
-            {
-              username = "your-username";
-            }
-          '');
-
       system = "aarch64-darwin";
 
       # 共通の darwinSystem 構成を生成する関数を定義する
