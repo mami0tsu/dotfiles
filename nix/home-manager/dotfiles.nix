52e6cce8a99d70836d0812aa2561de17dfe7e9b7
mami0tsu <mami0tsu.jp@gmail.com>
2026-04-26 17:04:56 +0900

    feat(nix): remove obsolete brew and mise configurations and files

--- nix/home-manager/dotfiles.nix
+++ nix/home-manager/dotfiles.nix
@@ -8,14 +8,12 @@
   xdg.configFile = {
     "espanso".source = "${self}/espanso";
     "ghostty".source = "${self}/ghostty";
-    "mise".source = "${self}/mise";
     "nvim".source = "${self}/nvim";
     "starship".source = "${self}/starship";
     "zellij".source = "${self}/zellij";
   };
 
   home.file = {
-    ".brewfile".source = "${self}/brew/.brewfile";
     ".editorconfig".source = "${self}/.editorconfig";
   };
 }
