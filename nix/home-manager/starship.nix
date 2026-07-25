b428cb32e118d71043048a072f3d13a0de3ef4ef
mami0tsu <mami0tsu.jp@gmail.com>
2026-07-25 17:27:50 +0900

    chore: migrate Starship settings to Home Manager P-29

--- nix/home-manager/starship.nix
+++ nix/home-manager/starship.nix
@@ -5,5 +5,33 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      add_newline = true;
+      scan_timeout = 10;
+
+      battery.disabled = true;
+      cmd_duration.show_milliseconds = true;
+
+      character = {
+        error_symbol = "[>](bold red)";
+        format = "$symbol";
+        success_symbol = "[>](bold green)";
+      };
+
+      directory.format = "[$path]($style) ";
+
+      git_status = {
+        ahead = "\${count}↑";
+        behind = "\${count}↓";
+        conflicted = "\${count}=";
+        deleted = "\${count}x";
+        diverged = "\${count}⇕";
+        modified = "\${count}!";
+        renamed = "\${count}»";
+        staged = "[++\\($count\\)](green)";
+        stashed = "\${count}*";
+        untracked = "\${count}?";
+      };
+    };
   };
 }
